5b870e5e7e23129a0ec712d384f36156a7b07db2
ansi13 <ansimatt@gmail.com>
2020-07-16 14:58:59 +0530

    Security implementation notes_backend

--- notes_backend/NotesBackend/test/conftest.py
+++ notes_backend/NotesBackend/test/conftest.py
@@ -1,7 +1,12 @@
+from collections import defaultdict
 import pytest
 import http.client
-from notes.app import create_app
+
 from faker import Faker
+
+from .constants import PRIVATE_KEY
+from notes.app import create_app
+from notes import token_validation
 
 fake = Faker()
 
@@ -15,20 +20,57 @@ def app():
 @pytest.fixture
 def notes_fixture(client):
     note_ids = []
+    header = token_validation.generate_token_header(fake.name(),
+                                                    PRIVATE_KEY)
+    headers = {
+        'Authorization': header,
+    }
+
     for _ in range(3):
         note = {
             'text': fake.text(230)
         }
-        response = client.post('/api/notes/', data=note)
+        response = client.post('/api/me/notes/', data=note,
+                               headers=headers)
+
         assert response.status_code == http.client.CREATED
         result = response.json
         note_ids.append(result['id'])
-    yield note_ids
+    yield note_ids, headers
 
-    response = client.get('/api/notes/')
+    response = client.get('/api/me/notes/', headers=headers)
     notes = response.json
     for note in notes:
         note_id = note['id']
-        url = f'/api/notes/{note_id}/'
-        response = client.delete(url)
+        url = f'/api/me/notes/{note_id}/'
+        response = client.delete(url, headers=headers)
+        assert response.status_code == http.client.NO_CONTENT
+
+
+@pytest.fixture
+def notes_random_fixture(client):
+    note_ids = []
+    headers_dict = defaultdict(str)
+    for _ in range(3):
+        note = {
+            'text': fake.text(230)
+        }
+        header = token_validation.generate_token_header(fake.name(),
+                                                        PRIVATE_KEY)
+        headers = {
+            'Authorization': header,
+        }
+        response = client.post('/api/me/notes/', data=note,
+                               headers=headers)
+
+        assert response.status_code == http.client.CREATED
+        result = response.json
+        note_ids.append(result['id'])
+        headers_dict[result['id']] = header
+    yield note_ids
+
+    for note_id in note_ids:
+        header = headers_dict[note_id]
+        url = f'/api/me/notes/{note_id}/'
+        response = client.delete(url, headers={'Authorization': header,})
         assert response.status_code == http.client.NO_CONTENT
--- notes_backend/NotesBackend/test/test_notes.py
+++ notes_backend/NotesBackend/test/test_notes.py
@@ -1,22 +1,32 @@
 from unittest.mock import ANY
 import http.client
+
 from freezegun import freeze_time
 from faker import Faker
 
+from .constants import PRIVATE_KEY, PUBLIC_KEY
+from notes import token_validation
 fake = Faker()
 
 
 @freeze_time('2019-05-07 13:47:34')
-def test_create_thought(client):
+def test_create_note(client):
     new_note = {
         'text': fake.text(240),
     }
-    response = client.post('/api/notes/', data=new_note)
+    header = token_validation.generate_token_header(fake.name(),
+                                                    PRIVATE_KEY)
+    headers = {
+        'Authorization': header,
+    }
+    response = client.post('/api/me/notes/', data=new_note,
+                           headers=headers)
     result = response.json
     assert response.status_code == http.client.CREATED
 
     expected = {
         'id': ANY,
+        'username': ANY,
         'text': new_note['text'],
         'time_created': '2019-05-07T13:47:34',
         'time_modified': '2019-05-07T13:47:34',
@@ -24,56 +34,148 @@ def test_create_thought(client):
     assert result == expected
 
 
-def test_list_all_notes(client):
-    response = client.get('/api/notes/')
+def test_create_note_unauthorized(client):
+    new_note = {
+        'text': fake.text(240),
+    }
+    response = client.post('/api/me/notes/', data=new_note)
+    assert http.client.UNAUTHORIZED == response.status_code
+
+
+def test_list_all_notes(client, notes_fixture):
+    note_ids, headers = notes_fixture
+    username = \
+        token_validation.validate_token_header(headers['Authorization'],
+                                               PUBLIC_KEY)
+
+    response = client.get('/api/me/notes/', headers=headers)
     result = response.json
 
     assert response.status_code == http.client.OK
-    assert len(result) > 0
+    assert len(result) == len(note_ids)
 
-    # Check that the ids are increasing
-    previous_id = -1
     for note in result:
         expected = {
+            'username': username,
             'text': ANY,
             'id': ANY,
             'time_created': ANY,
             'time_modified': ANY,
         }
         assert expected == note
-        assert note['id'] > previous_id
-        previous_id = note['id']
+
+
+def test_search_notes(client):
+    username = fake.name()
+    new_note = {
+        'text': 'dummy text for search test',
+    }
+    header = token_validation.generate_token_header(username,
+                                                    PRIVATE_KEY)
+    headers = {
+        'Authorization': header,
+    }
+    response = client.post('/api/me/notes/', data=new_note,
+                           headers=headers)
+    assert response.status_code == http.client.CREATED
+    response = client.get('/api/me/notes/?search=dummy', headers=headers)
+
+    assert response.status_code == http.client.OK
+    result = response.json
+
+    assert len(result) > 0
+    for note in result:
+        expected = {
+            'text': ANY,
+            'username': username,
+            'id': ANY,
+            'time_created': ANY,
+            'time_modified': ANY
+        }
+        assert expected == note
+        assert 'dummy' in note['text'].lower()
 
 
 def test_view_note(client, notes_fixture):
-    note_id = notes_fixture[0]
-    response = client.get(f'/api/notes/{note_id}/')
+    note_ids, headers = notes_fixture
+    username = \
+        token_validation.validate_token_header(headers['Authorization'],
+                                               PUBLIC_KEY)
+    response = client.get(f'/api/me/notes/{note_ids[0]}/',
+                          headers=headers)
     result = response.json
 
     assert http.client.OK == response.status_code
+    assert result['username'] == username
     assert 'text' in result
     assert 'time_modified' in result
     assert 'time_created' in result
-    assert 'id' in result
+    assert result['id'] == note_ids[0]
+
+
+def test_view_note_existing_unauthorized(client, notes_random_fixture):
+    note_ids = notes_random_fixture
+    header = \
+        token_validation.generate_token_header('dummy user',
+                                               PRIVATE_KEY)
+    headers = {
+        'Authorization': header,
+    }
+    response = client.get(f'/api/me/notes/{note_ids[0]}/',
+                          headers=headers)
+
+    assert http.client.NOT_FOUND == response.status_code
 
 
 def test_update_note(client, notes_fixture):
-    note_id = notes_fixture[1]
+    note_ids, headers = notes_fixture
     text = {
         'text': fake.text(250)
     }
 
-    response = client.put(f'/api/notes/{note_id}/', data=text)
+    response = client.put(f'/api/me/notes/{note_ids[1]}/', data=text,
+                          headers=headers)
     old_result = response.json
     assert http.client.OK == response.status_code
 
-    new_response = client.get(f'/api/notes/{note_id}/')
+    new_response = client.get(f'/api/me/notes/{note_ids[1]}/',
+                              headers=headers)
+    new_result = new_response.json
+    assert old_result == new_result
+
+
+def test_update_note_existing_unauthorized(client, notes_random_fixture):
+    note_ids = notes_random_fixture
+    text = {
+        'text': fake.text(250)
+    }
+    header = \
+        token_validation.generate_token_header('dummy user',
+                                               PRIVATE_KEY)
+    headers = {
+        'Authorization': header,
+    }
+
+    response = client.put(f'/api/me/notes/{note_ids[1]}/', data=text,
+                          headers=headers)
+    old_result = response.json
+    assert (http.client.NOT_FOUND == response.status_code or
+            http.client.UNAUTHORIZED == response.status_code)
+
+    new_response = client.get(f'/api/me/notes/{note_ids[1]}/',
+                              headers=headers)
     new_result = new_response.json
     assert old_result == new_result
 
 
 def test_get_non_existent_note(client):
     note_id = 32423
-    response = client.get(f'/api/notes/{note_id}/')
+    header = token_validation.generate_token_header(fake.name(),
+                                                    PRIVATE_KEY)
+    headers = {
+        'Authorization': header,
+    }
+    response = client.get(f'/api/me/notes/{note_id}/',
+                          headers=headers)
 
     assert response.status_code == http.client.NOT_FOUND
