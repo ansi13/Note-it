f9943f9f1b314d6510a8cceebed4c7e756374b8a
ansi13 <ansimatt@gmail.com>
2020-07-31 18:11:40 +0530

    Fix Static Analysis & password storage length

--- users_backend/UsersBackend/users/api_namespace.py
+++ users_backend/UsersBackend/users/api_namespace.py
@@ -7,32 +7,32 @@ from flask_restx import Namespace, Resource, fields
 from users import config
 from users.db import db
 from users.models import UserModel
-from users.token_validation import generate_token_header,\
-    validate_token_header
+from users.token_validation import generate_token_header
 
 
-api_namespace = Namespace('api', description='API operations')
+api_namespace = Namespace("api", description="API operations")
 
 model = {
-    'user_id': fields.Integer(),
-    'username': fields.String(),
+    "user_id": fields.Integer(),
+    "username": fields.String(),
     # DO NOT RETURN THE PASSWORD!!!
-    'time_created': fields.DateTime(),
+    "time_created": fields.DateTime(),
 }
-user_model = api_namespace.model('User', model)
+user_model = api_namespace.model("User", model)
 
 
 login_parser = api_namespace.parser()
-login_parser.add_argument('username', type=str, required=True,
-                          help='Username', location='form')
-login_parser.add_argument('password', type=str, required=True,
-                          help='Password', location='form')
+login_parser.add_argument(
+    "username", type=str, required=True, help="Username", location="form"
+)
+login_parser.add_argument(
+    "password", type=str, required=True, help="Password", location="form"
+)
 
 
-@api_namespace.route('/login/')
+@api_namespace.route("/login/")
 class UserLogin(Resource):
-
-    @api_namespace.doc('login')
+    @api_namespace.doc("login")
     @api_namespace.expect(login_parser)
     def post(self):
         """
@@ -41,26 +41,25 @@ class UserLogin(Resource):
         args = login_parser.parse_args()
 
         # Search for the user
-        user = (UserModel
-                .query
-                .filter(UserModel.username == args['username'])
-                .first())
+        user = UserModel.query.filter(
+            UserModel.username == args["username"]
+        ).first()
         if not user:
-            return '', http.client.UNAUTHORIZED
+            return "", http.client.UNAUTHORIZED
 
         # Check the password
-        # REMEMBER, THIS IS NOT SAFE. DO NOT STORE PASSWORDS IN PLAIN
-        if not (bcrypt.checkpw(args['password'].encode('utf-8'), user.password)):
-            return '', http.client.UNAUTHORIZED
+        if not (
+            bcrypt.checkpw(args["password"].encode("utf-8"), user.password)
+        ):
+            return "", http.client.UNAUTHORIZED
 
         # Generate the header
         header = generate_token_header(user.username, config.PRIVATE_KEY)
-        return {'Authorized': header}, http.client.OK
+        return {"Authorized": header}, http.client.OK
 
 
-@api_namespace.route('/signup/')
+@api_namespace.route("/signup/")
 class UserCreate(Resource):
-
     @api_namespace.expect(login_parser)
     @api_namespace.marshal_with(user_model, code=http.client.CREATED)
     def post(self):
@@ -70,11 +69,13 @@ class UserCreate(Resource):
         args = login_parser.parse_args()
 
         salt = bcrypt.gensalt(rounds=12)
-        hashed = bcrypt.hashpw(args['password'].encode('utf-8'), salt)
+        hashed = bcrypt.hashpw(args["password"].encode("utf-8"), salt)
 
-        new_user = UserModel(username=args['username'],
-                             password=hashed,
-                             time_created=datetime.utcnow())
+        new_user = UserModel(
+            username=args["username"],
+            password=hashed,
+            time_created=datetime.utcnow(),
+        )
         db.session.add(new_user)
         db.session.commit()
 
--- users_backend/UsersBackend/users/models.py
+++ users_backend/UsersBackend/users/models.py
@@ -5,5 +5,5 @@ from users.db import db
 class UserModel(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
-    password = db.Column(db.String(100))
+    password = db.Column(db.String)
     time_created = db.Column(db.DateTime, server_default=func.now())
